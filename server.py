dc9987e3a2b3537fa1187cec5d9bd669cfdf33cb
emart126 <125086504+emart126@users.noreply.github.com>
2025-01-30 09:36:29 -0800

    added SQL Connection to the server

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from flask import Flask
-# from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'password'
-# app.config['MYSQL_DB'] = 'HikeReviewDB'
-# mysql = MySQL(app)
-# cursor = mysql.connection.cursor()
+# Enable CORS for the entire app
+CORS(app)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '---'
+app.config['MYSQL_DB'] = 'hikereview'
+mysql = MySQL(app)
 
 # Setup route pages
 @app.route("/")
@@ -20,12 +23,21 @@ def getData():
     # Return template data as JSON
     return {"Users": ["Bob", "Alice", "Chris"]}
 
-    # MySQL Temp fetching
-    # cur = mysql.connection.cursor()
-    # cur.execute('SELECT * FROM dataRecord')
-    # dataObj = cur.fetchall()
-    # cur.close()
-    # return f"d: {dataObj}"
+@app.route("/MySQLdata")
+def getMySQLData():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    usersString = ''
+    for record in users:
+        usersString += 'Id: ' + str(record[0]) + '\n'
+        usersString += 'Username: ' + str(record[1]) + '\n'
+        usersString += 'Email: ' + str(record[2]) + '\n'
+        usersString += 'Password: ' + str(record[3]) + '\n'
+        usersString += 'Created At: ' + str(record[4]) + '\n'
+    
+    cursor.close()
+    return usersString
 
 if __name__ == "__main__":
     app.run(debug=True)
