98d25c2ce29d20db09aaa473e177e0476f0107a2
emart126 <125086504+emart126@users.noreply.github.com>
2025-02-20 18:57:32 -0800

    removed test database config

--- server.py
+++ server.py
@@ -8,18 +8,12 @@ app = Flask(__name__)
 
 CORS(app)
 
-# Test Database
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'securePassword!@#123'
-app.config['MYSQL_DB'] = 'hikereview'
-# app.config['SECRET_KEY'] = os.urandom(24) # Session management
-
 # Main AWS Database
-# app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-# app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-# app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-# app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
 mysql = MySQL(app)
 
 # User Datasctructure
