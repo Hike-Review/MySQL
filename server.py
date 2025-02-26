a09ec2685783945d720eddf7400fc72855fa14b8
emart126 <125086504+emart126@users.noreply.github.com>
2025-02-25 17:56:49 -0800

    fixed app host ip using debugging version

--- server.py
+++ server.py
@@ -330,5 +330,4 @@ def getHikeData():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  # Default to 8080 if not set
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug = True)
+    app.run(host="0.0.0.0", port=port)
